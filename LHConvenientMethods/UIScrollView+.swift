904865c40e5f27c8797632e9ef06595dc18222be
Li-Heng Hsu <yesleon@me.com>
2018-11-04 16:41:46 +0800

    Add UIScrollView+KeyboardHandling

---
+++ LHConvenientMethods/UIScrollView+.swift
@@ -0,0 +1,42 @@
+//
+//  UIScrollView+.swift
+//  LHConvenientMethods
+//
+//  Created by 許立衡 on 2018/11/4.
+//  Copyright © 2018 narrativesaw. All rights reserved.
+//
+
+import Foundation
+
+extension UIScrollView {
+    
+    public func setHandlesKeyboard(_ handlesKeyboard: Bool) {
+        if handlesKeyboard {
+            NotificationCenter.default.removeObserver(self, name: UIWindow.keyboardDidChangeFrameNotification, object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardFrameDidChange), name: UIWindow.keyboardDidChangeFrameNotification, object: nil)
+        } else {
+            NotificationCenter.default.removeObserver(self, name: UIWindow.keyboardDidChangeFrameNotification, object: nil)
+        }
+    }
+    
+    @objc private func keyboardFrameDidChange(notification: Notification) {
+        let kbFrame = notification.userInfo![UIWindow.keyboardFrameEndUserInfoKey] as! CGRect
+        let viewFrame: CGRect
+        if let window = window {
+            viewFrame = convert(bounds, to: window)
+        } else {
+            viewFrame = frame
+        }
+        let overlap = kbFrame.intersection(viewFrame)
+        if !overlap.isEmpty {
+            let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: overlap.height - safeAreaInsets.bottom, right: 0)
+            contentInset = contentInsets
+            scrollIndicatorInsets = contentInsets
+        } else {
+            let contentInsets = UIEdgeInsets.zero
+            contentInset = contentInsets
+            scrollIndicatorInsets = contentInsets
+        }
+    }
+    
+}
