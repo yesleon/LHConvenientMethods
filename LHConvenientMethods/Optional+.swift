c5aa16c64c7ac1be94ed06afd1acbf586bf92be6
Li-Heng Hsu <yesleon@me.com>
2018-11-13 14:31:38 +0800

    Add Optional+

---
+++ LHConvenientMethods/Optional+.swift
@@ -0,0 +1,19 @@
+//
+//  Optional+.swift
+//  LHConvenientMethods
+//
+//  Created by 許立衡 on 2018/11/13.
+//  Copyright © 2018 narrativesaw. All rights reserved.
+//
+
+import Foundation
+
+extension Optional {
+    
+    public mutating func remove() -> Wrapped? {
+        let returnValue = self
+        self = nil
+        return returnValue
+    }
+    
+}
