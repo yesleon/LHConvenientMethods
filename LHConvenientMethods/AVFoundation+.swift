1bb602a19c6bb3583c30ef71b7ba31cbe3aefcd4
Li-Heng Hsu <yesleon@me.com>
2018-11-27 15:31:35 +0800

    Add AVFoundation+

---
+++ LHConvenientMethods/AVFoundation+.swift
@@ -0,0 +1,58 @@
+//
+//  AVFoundation+.swift
+//  LHConvenientMethods
+//
+//  Created by 許立衡 on 2018/11/27.
+//  Copyright © 2018 narrativesaw. All rights reserved.
+//
+
+import AVFoundation
+
+
+extension AVCaptureDevice {
+    public static func defaultCamera(position: Position) throws -> AVCaptureDevice {
+        guard let device = `default`(.builtInWideAngleCamera, for: .video, position: position) else { throw Error.cannotFindDevice }
+        return device
+    }
+    public enum Error: Swift.Error {
+        case cannotFindDevice
+    }
+}
+
+extension AVCaptureSession {
+    
+    public enum Error: Swift.Error {
+        case cannotAddInput, cannotAddOutput
+    }
+    
+    public func configure(configureHandler: (AVCaptureSession) throws -> Void) rethrows {
+        beginConfiguration()
+        try configureHandler(self)
+        commitConfiguration()
+    }
+    
+    public func setInputVideoDevice(_ device: AVCaptureDevice) throws {
+        let videoDeviceInput = try AVCaptureDeviceInput(device: device)
+        guard canAddInput(videoDeviceInput) else { throw Error.cannotAddInput }
+        addInput(videoDeviceInput)
+    }
+    
+    public func setOutput(_ output: AVCaptureOutput) throws {
+        guard canAddOutput(output) else { throw Error.cannotAddOutput }
+        addOutput(output)
+    }
+    
+}
+
+extension AVCaptureVideoOrientation {
+    
+    public init?(_ deviceOrientation: UIDeviceOrientation) {
+        let rawValue = UIDevice.current.orientation.rawValue
+        if rawValue >= 1, rawValue <= 4 {
+            self.init(rawValue: rawValue)
+        } else {
+            return nil
+        }
+    }
+    
+}
--- LHConvenientMethods/Declarative.swift
+++ LHConvenientMethods/Declarative.swift
@@ -24,6 +24,20 @@ extension Declarative {
     
 }
 
+extension Declarative where Self: NSObject {
+    
+    public init(configureHanler: (Self) -> Void) {
+        self.init()
+        configureHanler(self)
+    }
+    
+    public init(configureHanler: (Self) throws -> Void) rethrows {
+        self.init()
+        try configureHanler(self)
+    }
+    
+}
+
 extension Declarative where Self: UIView {
     
     public static func animateObject<T>(object: T, withDuration duration: TimeInterval, delay: TimeInterval, options: UIView.AnimationOptions = [], animations: @escaping (T) -> Void, completion: ((T, Bool) -> Void)? = nil) {
